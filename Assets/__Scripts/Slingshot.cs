4df65dbeaf01800eceb19722ba461e103eeb15d5
ellise-chan <49345342+ellise-chan@users.noreply.github.com>
2019-10-03 11:34:59 -0400

    update MissionDemolition (1)

--- Assets/__Scripts/Slingshot.cs
+++ Assets/__Scripts/Slingshot.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 // YOU must implement the Slingshot
 
 public class Slingshot : MonoBehaviour {
+    static private Slingshot S;
     [Header("Set in Inspector")]
     public GameObject prefabProjectile;
     public float velocityMult = 8f;
@@ -17,8 +18,18 @@ public class Slingshot : MonoBehaviour {
     public bool aimingMode;
     private Rigidbody projectileRigidbody;
 
+    static public Vector3 LAUNCH_POS
+    {
+        get
+        {
+            if (S == null) return Vector3.zero;
+            return S.launchPos;
+        }
+    }
+
     private void Awake()
     {
+        S = this;
         Transform launchPointTrans = transform.FindChild("LaunchPoint");
         launchPoint = launchPointTrans.gameObject;
         launchPoint.SetActive(false);
